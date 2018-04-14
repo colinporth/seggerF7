d7c482be193731edfef90aa8a0be8cc7c09e2e3a
colinporth <colinporth@users.noreply.github.com>
2018-04-14 15:05:14 +0100

    fiddle

--- usbMsc/main.cpp
+++ usbMsc/main.cpp
@@ -856,14 +856,14 @@ void cApp::setJpegHeader (int width, int height, int qscale) {
 //}}}
 
 //{{{
-void MPU_Config()
-{
-  MPU_Region_InitTypeDef MPU_InitStruct;
+void MPU_Config() {
 
-  /* Disable the MPU */
+  // Disable the MPU
   HAL_MPU_Disable();
 
-  /* Configure the MPU attributes as WT for SRAM */
+  // Configure the MPU attributes as WT for SRAM
+  MPU_Region_InitTypeDef MPU_InitStruct;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
   MPU_InitStruct.BaseAddress = 0x20010000;
   MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
@@ -871,14 +871,13 @@ void MPU_Config()
   MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
   MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
   MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
   MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
   MPU_InitStruct.SubRegionDisable = 0x00;
   MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+  HAL_MPU_ConfigRegion (&MPU_InitStruct);
 
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
-
-  /* Configure the MPU as Normal Non Cacheable for Ethernet Buffers in the SRAM2 */
+  // Configure the MPU as Normal Non Cacheable for Ethernet Buffers in the SRAM2
+  MPU_InitStruct.Number = MPU_REGION_NUMBER1;
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
   MPU_InitStruct.BaseAddress = 0x2004C000;
   MPU_InitStruct.Size = MPU_REGION_SIZE_16KB;
@@ -886,14 +885,13 @@ void MPU_Config()
   MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
   MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
   MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER1;
   MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
   MPU_InitStruct.SubRegionDisable = 0x00;
   MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+  HAL_MPU_ConfigRegion (&MPU_InitStruct);
 
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
-
-  /* Configure the MPU as Device for Ethernet Descriptors in the SRAM2 */
+  // Configure the MPU as Device for Ethernet Descriptors in the SRAM2
+  MPU_InitStruct.Number = MPU_REGION_NUMBER2;
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
   MPU_InitStruct.BaseAddress = 0x2004C000;
   MPU_InitStruct.Size = MPU_REGION_SIZE_256B;
@@ -901,14 +899,12 @@ void MPU_Config()
   MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
   MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
   MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER2;
   MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
   MPU_InitStruct.SubRegionDisable = 0x00;
   MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+  HAL_MPU_ConfigRegion (&MPU_InitStruct);
 
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
-
-  /* Enable the MPU */
+  // Enable the MPU
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
   }
 //}}}
