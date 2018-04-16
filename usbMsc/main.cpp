962c1a1aabe35ba6774049ee4df80f521c31aca3
colinporth <colinporth@users.noreply.github.com>
2018-04-16 11:23:36 +0100

    fidle

--- usbMsc/main.cpp
+++ usbMsc/main.cpp
@@ -25,7 +25,7 @@
 //}}}
 #define USE_CAMERA
 
-const char* kVersion = "WebCam 15/4/18";
+const char* kVersion = "WebCam 16/4/18";
 
 uint16_t* kRgb565Buffer = (uint16_t*)0xc0100000;
 uint8_t*  kJpegBuffer   =  (uint8_t*)0xc0200000;
@@ -988,62 +988,8 @@ void netThread (void* arg) {
 //}}}
 
 //{{{
-void MPU_Config() {
-
-  // Disable the MPU
-  HAL_MPU_Disable();
-
-  // Configure the MPU attributes as WT for SRAM
-  MPU_Region_InitTypeDef MPU_InitStruct;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.BaseAddress = 0x20010000;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
-  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-  MPU_InitStruct.SubRegionDisable = 0x00;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
-  HAL_MPU_ConfigRegion (&MPU_InitStruct);
-
-  // Configure the MPU as Normal Non Cacheable for Ethernet Buffers in the SRAM2
-  MPU_InitStruct.Number = MPU_REGION_NUMBER1;
-  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.BaseAddress = 0x2004C000;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_16KB;
-  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
-  MPU_InitStruct.SubRegionDisable = 0x00;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
-  HAL_MPU_ConfigRegion (&MPU_InitStruct);
-
-  // Configure the MPU as Device for Ethernet Descriptors in the SRAM2
-  MPU_InitStruct.Number = MPU_REGION_NUMBER2;
-  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.BaseAddress = 0x2004C000;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_256B;
-  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-  MPU_InitStruct.SubRegionDisable = 0x00;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
-  HAL_MPU_ConfigRegion (&MPU_InitStruct);
-
-  // Enable the MPU
-  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-  }
-//}}}
-//{{{
 int main() {
 
-  //MPU_Config();
   SCB_EnableICache();
   SCB_EnableDCache();
 
