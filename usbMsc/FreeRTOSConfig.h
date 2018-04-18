94dc3436236d66ce2a1e6953429767ca322d0616
colinporth <colinporth@users.noreply.github.com>
2018-04-18 15:27:20 +0100

    fiddlimn

--- usbMsc/FreeRTOSConfig.h
+++ usbMsc/FreeRTOSConfig.h
@@ -1,33 +1,27 @@
 #pragma once
-
 #include <stdint.h>
+
 extern uint32_t SystemCoreClock;
 
-#define configUSE_PREEMPTION    1
-#define configUSE_IDLE_HOOK     1
-#define configUSE_TICK_HOOK     1 
-#define traceTASK_SWITCHED_IN()  extern void StartIdleMonitor(void); \
-																 StartIdleMonitor()
-#define traceTASK_SWITCHED_OUT() extern void EndIdleMonitor(void); \
-																 EndIdleMonitor()
+#define configUSE_PREEMPTION      1
 
+#define configUSE_IDLE_HOOK       1
+#define configUSE_TICK_HOOK       1
+#define traceTASK_SWITCHED_IN()   extern void StartIdleMonitor(); StartIdleMonitor()
+#define traceTASK_SWITCHED_OUT()  extern void EndIdleMonitor(); EndIdleMonitor()
 
-#define configCPU_CLOCK_HZ      ( SystemCoreClock )
-#define configTICK_RATE_HZ      ( ( TickType_t ) 1000 )
+#define configCPU_CLOCK_HZ        (SystemCoreClock)
+#define configTICK_RATE_HZ        ((TickType_t)1000)
 
-#define configMAX_PRIORITIES      (  7 )
-#define configMINIMAL_STACK_SIZE    ( ( uint16_t ) 128 )
-#if defined(__GNUC__)
- #define configTOTAL_HEAP_SIZE      ( ( size_t ) ( 128 * 1024 ) )
-#else
- #define configTOTAL_HEAP_SIZE      ( ( size_t ) ( 128 * 1024 ) )
-#endif
+#define configMAX_PRIORITIES      (7)
+#define configMINIMAL_STACK_SIZE  ((uint16_t)128)
+#define configTOTAL_HEAP_SIZE     ((size_t)(128 * 1024))
 
-#define configMAX_TASK_NAME_LEN     (8)
-#define configUSE_TRACE_FACILITY    1
-#define configUSE_16_BIT_TICKS      0
-#define configIDLE_SHOULD_YIELD     1
-#define configUSE_MUTEXES           1
+#define configMAX_TASK_NAME_LEN        (8)
+#define configUSE_TRACE_FACILITY        1
+#define configUSE_16_BIT_TICKS          0
+#define configIDLE_SHOULD_YIELD         1
+#define configUSE_MUTEXES               1
 #define configQUEUE_REGISTRY_SIZE       8
 #define configCHECK_FOR_STACK_OVERFLOW  0
 #define configUSE_RECURSIVE_MUTEXES     1
@@ -38,24 +32,24 @@ extern uint32_t SystemCoreClock;
 #define configUSE_STATS_FORMATTING_FUNCTIONS  1
 
 // Co-routine definitions
-#define configUSE_CO_ROUTINES             0
-#define configMAX_CO_ROUTINE_PRIORITIES   (2)
+#define configUSE_CO_ROUTINES            0
+#define configMAX_CO_ROUTINE_PRIORITIES (2)
 
 // Software timer definitions.
-#define configUSE_TIMERS              0
+#define configUSE_TIMERS               0
 #define configTIMER_TASK_PRIORITY     (2)
 #define configTIMER_QUEUE_LENGTH      10
 #define configTIMER_TASK_STACK_DEPTH  (configMINIMAL_STACK_SIZE * 2)
 
 // Set the following definitions to 1 to include the API function, or zero to exclude the API function.
-#define INCLUDE_vTaskPrioritySet    1
-#define INCLUDE_uxTaskPriorityGet   1
-#define INCLUDE_vTaskDelete         1
-#define INCLUDE_vTaskCleanUpResources 0
-#define INCLUDE_vTaskSuspend          0
-#define INCLUDE_vTaskDelayUntil       0
-#define INCLUDE_vTaskDelay            1
-#define INCLUDE_xTaskGetSchedulerState  1
+#define INCLUDE_vTaskPrioritySet       1
+#define INCLUDE_uxTaskPriorityGet      1
+#define INCLUDE_vTaskDelete            1
+#define INCLUDE_vTaskCleanUpResources  0
+#define INCLUDE_vTaskSuspend           0
+#define INCLUDE_vTaskDelayUntil        0
+#define INCLUDE_vTaskDelay             1
+#define INCLUDE_xTaskGetSchedulerState 1
 
 // Cortex-M specific definitions.
 #ifdef __NVIC_PRIO_BITS
