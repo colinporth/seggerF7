a56b3b6a464063b3d5541e4c320fa078b89dc5ab
colinporth <colinporth@users.noreply.github.com>
2018-04-27 10:52:35 +0100

    remoive crud

--- BSP/sd.cpp
+++ BSP/sd.cpp
@@ -23,10 +23,6 @@
 #define SD_DMAx_Tx_IRQn              DMA2_Stream6_IRQn
 #define SD_DMAx_Rx_IRQn              DMA2_Stream3_IRQn
 
-#define BSP_SDMMC_IRQHandler         SDMMC1_IRQHandler
-#define BSP_SDMMC_DMA_Tx_IRQHandler  DMA2_Stream6_IRQHandler
-#define BSP_SDMMC_DMA_Rx_IRQHandler  DMA2_Stream3_IRQHandler
-
 #define SD_DATATIMEOUT  ((uint32_t)100000000)
 
 #define SD_TRANSFER_OK    ((uint8_t)0x00)
@@ -34,37 +30,33 @@
 
 #define SD_PRESENT      ((uint8_t)0x01)
 #define SD_NOT_PRESENT  ((uint8_t)0x00)
-
-#define MSD_OK                    ((uint8_t)0x00)
-#define MSD_ERROR                 ((uint8_t)0x01)
-#define MSD_ERROR_SD_NOT_PRESENT  ((uint8_t)0x02)
 //}}}
 
 bool gDebug = false;
 volatile DSTATUS gStat = STA_NOINIT;
 osMessageQId gSdQueueId;
 
-SD_HandleTypeDef uSdHandle;
-DMA_HandleTypeDef dma_rx_handle;
-DMA_HandleTypeDef dma_tx_handle;
+SD_HandleTypeDef gSdHandle;
+DMA_HandleTypeDef gDmaRxHandle;
+DMA_HandleTypeDef gDmaTxHandle;
 
 extern "C" {
-  void BSP_SDMMC_IRQHandler() { HAL_SD_IRQHandler (&uSdHandle); }
-  void BSP_SDMMC_DMA_Tx_IRQHandler() { HAL_DMA_IRQHandler (uSdHandle.hdmatx); }
-  void BSP_SDMMC_DMA_Rx_IRQHandler() { HAL_DMA_IRQHandler (uSdHandle.hdmarx); }
+  void SDMMC1_IRQHandler() { HAL_SD_IRQHandler (&gSdHandle); }
+  void DMA2_Stream6_IRQHandler() { HAL_DMA_IRQHandler (gSdHandle.hdmatx); }
+  void DMA2_Stream3_IRQHandler() { HAL_DMA_IRQHandler (gSdHandle.hdmarx); }
   }
 
 //{{{
 bool init() {
 
   // uSD device interface configuration
-  uSdHandle.Instance = SDMMC1;
-  uSdHandle.Init.ClockEdge           = SDMMC_CLOCK_EDGE_RISING;
-  uSdHandle.Init.ClockBypass         = SDMMC_CLOCK_BYPASS_DISABLE;
-  uSdHandle.Init.ClockPowerSave      = SDMMC_CLOCK_POWER_SAVE_DISABLE;
-  uSdHandle.Init.BusWide             = SDMMC_BUS_WIDE_1B;
-  uSdHandle.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
-  uSdHandle.Init.ClockDiv            = SDMMC_TRANSFER_CLK_DIV;
+  gSdHandle.Instance = SDMMC1;
+  gSdHandle.Init.ClockEdge           = SDMMC_CLOCK_EDGE_RISING;
+  gSdHandle.Init.ClockBypass         = SDMMC_CLOCK_BYPASS_DISABLE;
+  gSdHandle.Init.ClockPowerSave      = SDMMC_CLOCK_POWER_SAVE_DISABLE;
+  gSdHandle.Init.BusWide             = SDMMC_BUS_WIDE_1B;
+  gSdHandle.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
+  gSdHandle.Init.ClockDiv            = SDMMC_TRANSFER_CLK_DIV;
 
   // card inserted
   //{{{  GPIO configuration in input for uSD_Detect signal
@@ -106,40 +98,38 @@ bool init() {
   //}}}
 
   //{{{  Configure DMA Rx parameters
-  dma_rx_handle.Instance = SD_DMAx_Rx_STREAM;
-  dma_rx_handle.Init.Channel             = SD_DMAx_Rx_CHANNEL;
-  dma_rx_handle.Init.Direction           = DMA_PERIPH_TO_MEMORY;
-  dma_rx_handle.Init.PeriphInc           = DMA_PINC_DISABLE;
-  dma_rx_handle.Init.MemInc              = DMA_MINC_ENABLE;
-  dma_rx_handle.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
-  dma_rx_handle.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
-  dma_rx_handle.Init.Mode                = DMA_PFCTRL;
-  dma_rx_handle.Init.Priority            = DMA_PRIORITY_VERY_HIGH;
-  dma_rx_handle.Init.FIFOMode            = DMA_FIFOMODE_ENABLE;
-  dma_rx_handle.Init.FIFOThreshold       = DMA_FIFO_THRESHOLD_FULL;
-  dma_rx_handle.Init.MemBurst            = DMA_MBURST_INC4;
-  dma_rx_handle.Init.PeriphBurst         = DMA_PBURST_INC4;
-  __HAL_LINKDMA (&uSdHandle, hdmarx, dma_rx_handle);
-  HAL_DMA_DeInit(&dma_rx_handle);
-  HAL_DMA_Init(&dma_rx_handle);
+  gDmaRxHandle.Instance = SD_DMAx_Rx_STREAM;
+  gDmaRxHandle.Init.Channel             = SD_DMAx_Rx_CHANNEL;
+  gDmaRxHandle.Init.Direction           = DMA_PERIPH_TO_MEMORY;
+  gDmaRxHandle.Init.PeriphInc           = DMA_PINC_DISABLE;
+  gDmaRxHandle.Init.MemInc              = DMA_MINC_ENABLE;
+  gDmaRxHandle.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+  gDmaRxHandle.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
+  gDmaRxHandle.Init.Mode                = DMA_PFCTRL;
+  gDmaRxHandle.Init.Priority            = DMA_PRIORITY_VERY_HIGH;
+  gDmaRxHandle.Init.FIFOMode            = DMA_FIFOMODE_ENABLE;
+  gDmaRxHandle.Init.FIFOThreshold       = DMA_FIFO_THRESHOLD_FULL;
+  gDmaRxHandle.Init.MemBurst            = DMA_MBURST_INC4;
+  gDmaRxHandle.Init.PeriphBurst         = DMA_PBURST_INC4;
+  __HAL_LINKDMA (&gSdHandle, hdmarx, gDmaRxHandle);
+  HAL_DMA_Init (&gDmaRxHandle);
   //}}}
   //{{{  Configure DMA Tx parameters
-  dma_tx_handle.Instance = SD_DMAx_Tx_STREAM;
-  dma_tx_handle.Init.Channel             = SD_DMAx_Tx_CHANNEL;
-  dma_tx_handle.Init.Direction           = DMA_MEMORY_TO_PERIPH;
-  dma_tx_handle.Init.PeriphInc           = DMA_PINC_DISABLE;
-  dma_tx_handle.Init.MemInc              = DMA_MINC_ENABLE;
-  dma_tx_handle.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
-  dma_tx_handle.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
-  dma_tx_handle.Init.Mode                = DMA_PFCTRL;
-  dma_tx_handle.Init.Priority            = DMA_PRIORITY_VERY_HIGH;
-  dma_tx_handle.Init.FIFOMode            = DMA_FIFOMODE_ENABLE;
-  dma_tx_handle.Init.FIFOThreshold       = DMA_FIFO_THRESHOLD_FULL;
-  dma_tx_handle.Init.MemBurst            = DMA_MBURST_INC4;
-  dma_tx_handle.Init.PeriphBurst         = DMA_PBURST_INC4;
-  __HAL_LINKDMA(&uSdHandle, hdmatx, dma_tx_handle);
-  HAL_DMA_DeInit(&dma_tx_handle);
-  HAL_DMA_Init(&dma_tx_handle);
+  gDmaTxHandle.Instance = SD_DMAx_Tx_STREAM;
+  gDmaTxHandle.Init.Channel             = SD_DMAx_Tx_CHANNEL;
+  gDmaTxHandle.Init.Direction           = DMA_MEMORY_TO_PERIPH;
+  gDmaTxHandle.Init.PeriphInc           = DMA_PINC_DISABLE;
+  gDmaTxHandle.Init.MemInc              = DMA_MINC_ENABLE;
+  gDmaTxHandle.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+  gDmaTxHandle.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
+  gDmaTxHandle.Init.Mode                = DMA_PFCTRL;
+  gDmaTxHandle.Init.Priority            = DMA_PRIORITY_VERY_HIGH;
+  gDmaTxHandle.Init.FIFOMode            = DMA_FIFOMODE_ENABLE;
+  gDmaTxHandle.Init.FIFOThreshold       = DMA_FIFO_THRESHOLD_FULL;
+  gDmaTxHandle.Init.MemBurst            = DMA_MBURST_INC4;
+  gDmaTxHandle.Init.PeriphBurst         = DMA_PBURST_INC4;
+  __HAL_LINKDMA(&gSdHandle, hdmatx, gDmaTxHandle);
+  HAL_DMA_Init (&gDmaTxHandle);
   //}}}
 
   // NVIC configuration for DMA transfer complete interrupt
@@ -154,12 +144,12 @@ bool init() {
   HAL_NVIC_SetPriority (SDMMC1_IRQn, 0x0E, 0);
   HAL_NVIC_EnableIRQ (SDMMC1_IRQn);
 
-  if (HAL_SD_Init (&uSdHandle) != HAL_OK)
+  if (HAL_SD_Init (&gSdHandle) != HAL_OK)
     return false;
   cLcd::mLcd->debug (LCD_COLOR_YELLOW, "mspinit");
 
   // Enable wide operation
-  auto result = HAL_SD_ConfigWideBusOperation (&uSdHandle, SDMMC_BUS_WIDE_4B);
+  auto result = HAL_SD_ConfigWideBusOperation (&gSdHandle, SDMMC_BUS_WIDE_4B);
   cLcd::mLcd->debug (LCD_COLOR_YELLOW, "wide result %d", result);
 
   return result == HAL_OK;
@@ -182,7 +172,7 @@ void HAL_SD_RxCpltCallback (SD_HandleTypeDef* hsd) {
 DSTATUS checkStatus() {
 
   gStat = STA_NOINIT;
-  if (HAL_SD_GetCardState (&uSdHandle) == HAL_SD_CARD_TRANSFER)
+  if (HAL_SD_GetCardState (&gSdHandle) == HAL_SD_CARD_TRANSFER)
     gStat &= ~STA_NOINIT;
 
   return gStat;
@@ -227,19 +217,19 @@ DRESULT diskIoctl (BYTE cmd, void* buf) {
 
     // Get number of sectors on the disk (DWORD)
     case GET_SECTOR_COUNT :
-      HAL_SD_GetCardInfo (&uSdHandle, &CardInfo);
+      HAL_SD_GetCardInfo (&gSdHandle, &CardInfo);
       *(DWORD*)buf = CardInfo.LogBlockNbr;
       return RES_OK;
 
     // Get R/W sector size (WORD)
     case GET_SECTOR_SIZE :
-      HAL_SD_GetCardInfo (&uSdHandle, &CardInfo);
+      HAL_SD_GetCardInfo (&gSdHandle, &CardInfo);
       *(WORD*)buf = CardInfo.LogBlockSize;
       return RES_OK;
 
     // Get erase block size in unit of sector (DWORD)
     case GET_BLOCK_SIZE :
-      HAL_SD_GetCardInfo (&uSdHandle, &CardInfo);
+      HAL_SD_GetCardInfo (&gSdHandle, &CardInfo);
       *(DWORD*)buf = CardInfo.LogBlockSize / 512;
       return RES_OK;
 
@@ -260,13 +250,13 @@ DRESULT diskRead (const BYTE* buf, uint32_t sector, uint32_t numSectors) {
     return RES_ERROR;
     }
 
-  if (HAL_SD_ReadBlocks_DMA (&uSdHandle, (uint8_t*)buf, sector, numSectors) == HAL_OK) {
+  if (HAL_SD_ReadBlocks_DMA (&gSdHandle, (uint8_t*)buf, sector, numSectors) == HAL_OK) {
     osEvent event = osMessageGet (gSdQueueId, SD_TIMEOUT);
     if (event.status == osEventMessage) {
       if (event.value.v == READ_CPLT_MSG) {
         uint32_t timer = osKernelSysTick();
         while (timer < osKernelSysTick() + SD_TIMEOUT) {
-          if (HAL_SD_GetCardState (&uSdHandle) == HAL_SD_CARD_TRANSFER) {
+          if (HAL_SD_GetCardState (&gSdHandle) == HAL_SD_CARD_TRANSFER) {
             if (buf + (numSectors * 512) >= (uint8_t*)0x20010000) {
               uint32_t alignedAddr = (uint32_t)buf & ~0x1F;
               SCB_InvalidateDCache_by_Addr ((uint32_t*)alignedAddr, numSectors * 512 + ((uint32_t)buf - alignedAddr));
@@ -298,13 +288,13 @@ DRESULT diskWrite (const BYTE* buf, uint32_t sector, uint32_t numSectors) {
   SCB_CleanDCache_by_Addr ((uint32_t*)alignedAddr, (numSectors * 512) + ((uint32_t)buf - alignedAddr));
 
   auto ticks1 = osKernelSysTick();
-  if (HAL_SD_WriteBlocks_DMA (&uSdHandle, (uint8_t*)buf, sector, numSectors) == HAL_OK) {
+  if (HAL_SD_WriteBlocks_DMA (&gSdHandle, (uint8_t*)buf, sector, numSectors) == HAL_OK) {
     auto event = osMessageGet (gSdQueueId, SD_TIMEOUT);
     if (event.status == osEventMessage) {
       if (event.value.v == WRITE_CPLT_MSG) {
         auto ticks2 = osKernelSysTick();
         while (ticks2 < osKernelSysTick() + SD_TIMEOUT) {
-          if (HAL_SD_GetCardState (&uSdHandle) == HAL_SD_CARD_TRANSFER) {
+          if (HAL_SD_GetCardState (&gSdHandle) == HAL_SD_CARD_TRANSFER) {
             auto writeTime = ticks2 - ticks1;
             auto okTime = osKernelSysTick() - ticks2;
             if ((writeTime > 200) || (okTime > 200))
